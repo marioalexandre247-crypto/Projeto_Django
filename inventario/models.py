84320a29d980c6b50657a65a932f9707cff2ece2
Mario Alexandre <marioalexandre247@gmail.com>
2025-11-13 19:25:16 -0300

    Etapa 2 Concluida

--- inventario/models.py
+++ inventario/models.py
@@ -9,27 +9,27 @@ status_escolha = [
 
 #maxlength para limite de letras
 #unique garante que não terá categorias com nomes duplicados
-class categoria(models.Model):
+class Categoria(models.Model):
     nome = models.CharField(max_length=100, unique=True)
 
     class meta:
-        meta_name = 'categoria'
-        meta_name_plutal = 'categorias'
+        meta_name = 'Categoria'
+        meta_name_plutal = 'Categorias'
     
     def __str__(self):
-        return self.name
+        return self.nome
 #retorna o nome da categoria para facil visualização
 
 #O models.CharField é o tipo de campo mais fundamental do Django para armazenar texto de comprimento curto a médio no banco de dados.
-class equipamento(models.Model):
+class equipamento(models.CharField):
     nome = models.CharField(max_length=200)
     serial = models.CharField(max_length=100, unique=True)
     data = models.DateField()
-    categoria = models.ForeignKey(categoria, on_delete=models.PROTECT)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
     status = models.CharField(
         max_length=15,
-        choices='status_escolha',
-        default='EM_USO'
+        choices ='status_escolha',
+        default ='EM_USO'
     )
 
     class Meta:
