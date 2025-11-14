cfbc1d093b3e4e338a0a1ca90294f06eb1b02401
Mario Alexandre <marioalexandre247@gmail.com>
2025-11-13 21:37:09 -0300

    Etapa 3 Concluida

--- inventario/models.py
+++ inventario/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 
 # Create your models here.
-status_escolha = [
-    ('EM_USO', 'Em uso'),
-    ('ESTOQUE', 'Estoque'),
-    ('MANUTENCAO', 'Manutenção'),
-]
+
 
 #maxlength para limite de letras
 #unique garante que não terá categorias com nomes duplicados
@@ -21,14 +17,19 @@ class Categoria(models.Model):
 #retorna o nome da categoria para facil visualização
 
 #O models.CharField é o tipo de campo mais fundamental do Django para armazenar texto de comprimento curto a médio no banco de dados.
-class equipamento(models.CharField):
+class Equipamento(models.Model):
+    status_escolha = [
+    ('EM_USO', 'Em uso'),
+    ('ESTOQUE', 'Estoque'),
+    ('MANUTENCAO', 'Manutenção'),
+    ]
     nome = models.CharField(max_length=200)
     serial = models.CharField(max_length=100, unique=True)
     data = models.DateField()
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
     status = models.CharField(
         max_length=15,
-        choices ='status_escolha',
+        choices = status_escolha,
         default ='EM_USO'
     )
 
